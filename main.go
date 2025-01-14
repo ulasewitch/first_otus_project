0b34a776e2c926fb7a02ac360ac8665fb2c77a36
Qwerty <qwerty@ulasewitch.ru>
2025-01-14 17:45:15 +0300

    Ветка тестовая с фичей :)

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ func otus_print() {
 	fmt.Println(line, "это мой первый проект")
 }
 
+func otus_branch() {
+
+	line := "Привет,"
+	fmt.Println(line, "это новая фича")
+}
+
 func main() {
 	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
 	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
@@ -25,4 +31,5 @@ func main() {
 	}
 
 	otus_print()
+	otus_branch()
 }
